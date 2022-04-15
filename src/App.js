af203fb38af3530842a2045ab1cdf196c9d60394
Mejbah <mejba.s@gmail.com>
2022-04-16 03:38:49 +0600

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
-        <Route path='/' element={ }></Route>
-        <Route path='/about' element={ }></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
