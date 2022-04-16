800bd70f9c797d6a64ce31c7f316cf84c28af7e0
Mejbah <mejba.s@gmail.com>
2022-04-16 16:03:15 +0600

    Year and copyright setup

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const today = new Date();
+const year = today.getFullYear();
+
+
+
 const Footer = () => {
     return (
-        <footer>
-            <p><small>copyright @ /dynamic year/</small></p>
+        <footer className='text-center mt-5 bg-dark text-light py-5'>
+            <p><small>copyright &copy; {year}</small></p>
         </footer>
     );
 };
